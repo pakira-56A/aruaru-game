ec98cfce9199ecd6b21f42b693690dd4c2f6723a
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-06-07 03:12:04 +0900

    Rails generatorを設置、topsのルーティング・コントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # 元のメモ resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # 元のメモ get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  #　ここから編集
+  # トップページに移動する指示
+  root "tops#index"
 end
