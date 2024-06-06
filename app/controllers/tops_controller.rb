3499e336f049cd27b816387c15e2d951db701de3
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-06-07 07:22:43 +0900

    全ページにヘッダー/フッター配置、画像設置、ナビゲーションバー編集

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,7 @@
+# ルーティングに対応するコントローラーのアクションを定義
 class TopsController < ApplicationController
+
     # トップページに繋げる
-    def index
-    end
+    # Rubocopのルールに従い、何も行わないアクションは１行で書く(def~end)
+    def index; end
 end
