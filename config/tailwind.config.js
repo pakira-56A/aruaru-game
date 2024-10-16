fb8c51f64f36faee2d682a0920f4161806d56a34
pakira-56A <pakira.rrr@gmail.com>
2024-10-16 18:47:48 +0900

    お問い合わせのXアイコンを削除、背景色を黄色に戻し、ロゴを中央に

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'custom-yellow': '#fdea2e',
+      }
     },
   },
   plugins: [
