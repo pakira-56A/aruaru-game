57450b857b64656f132d67585ca9f354b4374c71
pakira-56A <pakira.rrr@gmail.com>
2024-10-20 00:19:46 +0900

    renderファイル追記

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,13 @@ set -o errexit
 
 bundle install
 
+# 10.19.追記
+yarn install
+yarn build
+yarn build:css
+
 # 卒制：Tailwindを導入追記
 bundle exec rails assets:clobber
 
 bundle exec rails assets:precompile
+
