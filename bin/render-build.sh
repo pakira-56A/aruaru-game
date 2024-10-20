d2e5df3a233ea0b2f5b047b482cfb1a4fd64dedb
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-10-20 14:34:41 +0900

    スタイルシートのリンクタグ２行は復活、yarnコマンド3行の位置を変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,13 +2,14 @@ set -o errexit
 
 bundle install
 
-# 10.19.追記
-yarn install
-yarn build
-yarn build:css_minify
 
 # 卒制：Tailwindを導入追記
 bundle exec rails assets:clobber
+
+# 10.19.追記（10.20.5行目から移動）
+yarn install
+yarn build
+yarn build:css_minify
 
 bundle exec rails assets:precompile
 
