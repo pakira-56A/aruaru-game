c12fdcc5b30490d797676f6668a200f10b953da9
pakira-56A <pakira.rrr@gmail.com>
2024-10-28 14:37:16 +0900

    GoogleLoginApiコントローラを実装

---
+++ app/controllers/google_login_api_controller.rb
@@ -0,0 +1,23 @@
+class GoogleLoginApiController < ApplicationController
+  require 'googleauth/id_tokens/verifier'
+
+  # g_csrf_tokenの検証がOKだった場合、callbackアクションが実行
+  protect_from_forgery except: :callback
+  # verify_g_csrf_tokenをメソッドを実行
+  before_action :verify_g_csrf_token
+
+  def callback
+    payload = Google::Auth::IDTokens.verify_oidc(params[:credential], aud: ENV['GOOGLE_CLIENT_ID'])
+    user = User.find_or_create_by(email: payload['email'])
+    session[:user_id] = user.id
+    redirect_to after_login_path, notice: 'ログインしました'
+  end
+
+  private
+
+  def verify_g_csrf_token
+    return unless cookies['g_csrf_token'].blank? || params[:g_csrf_token].blank? || cookies['g_csrf_token'] != params[:g_csrf_token]
+
+    redirect_to root_path, notice: '不正なアクセスです'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root "tops#index"
+
+  post "google_login_api/callback", to: "google_login_api#callback"
 end
