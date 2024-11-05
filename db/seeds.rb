d60799b44d64725f7dc309a18c16139ad67a3a5a
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-11-05 21:42:54 +0900

    ローカルにて、`db/seeds.rb`でユーザーと投稿レコードを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+ActiveRecord::Base.transaction do
+
+	10.times do |n|
+    User.create!(
+        name: "テスト#{n+1}",
+        email: "test#{n+1}@example.com",
+        password: "password")
+    end
+
+    user_ids = User.ids
+
+    20.times do |n|
+        user = User.find_by(id: user_ids.sample)
+        if user
+            user.posts.create!(
+                title: "タイトル#{n}",
+                aruaru_one: "あるある1#{n + 1}",
+                aruaru_two: "あるある2:#{n + 1}",
+                aruaru_three: "あるある3:#{n + 1}",
+                aruaru_four: "あるある4:#{n + 1}",
+                aruaru_five: "あるある5:#{n + 1}")
+        else
+            Rails.logger.error "ユーザーが見つからないエラー"
+        end
+    end
+end
+
+Rails.logger.debug "#{User.count}さんと、投稿「#{Post.count}」がDBに入った"
