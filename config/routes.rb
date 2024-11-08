a221ef5b024f590965bc48eb9e5332968fd79718
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-11-08 16:23:36 +0900

    Google認証#8 ：ログイン後の遷移先をルートに変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root "tops#toppage"
 
   post "google_login_api/callback", to: "google_login_api#callback"
-  get "/after_login", to: "tops#my_index"
+  get "/after_login", to: "tops#toppage" #アクセスされる時のため、一応残しています
 
   get "/policy", to: "tops#policy"
   get "/term", to: "tops#term"
