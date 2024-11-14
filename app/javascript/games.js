4dd717ecbb0c9f4ff7a1abad27dfcad7f1ab2806
pakira-56A <pakira.rrr@gmail.com>
2024-11-15 01:44:51 +0900

    ゲーム画面#2：カードに回転アニメ・クリック制限・文字に配色を追加

--- app/javascript/games.js
+++ app/javascript/games.js
@@ -55,12 +55,18 @@ function shuffle(arr) {
     return arr;
 }
 
+// ペア完成時の色を用意
+// 緑,青,ピンク,オレンジ,赤
+const colors = ['#21c000','#008eff', '#ff68fa', '#ff7b0b', '#ff0000'];
+let currentColorIndex = 0; // 現在の色のインデックス
+let isLocked = false; // ゲームがロックされているか否かを示すフラグ
+
 // クリック時の処理
 function turn(e) {
     var div = e.target;
 
-    // カードがすでに裏返されてるか、ゲームが終了してる場合は return
-    if (div.className === 'card' || div.className === 'card finish' || countUnit === 5) return;
+    // カードがすでに裏返されてる、ゲームが終了してる、またはロック中の場合は return
+    if (div.className === 'card' || div.className === 'card finish' || countUnit === 5 || isLocked) return;
 
     // カードを表にする
     div.className = 'card';
@@ -71,26 +77,36 @@ function turn(e) {
                     flgFirst = false; }
     // 2枚目の処理
     else {
-		    // 数字が1枚目と一致する場合
+        isLocked = true; // カードがめくれない制限（ロック）
+
+        // あるあるが1枚目とペアの場合
         if (cardFirst.number == div.number) {
             countUnit++;
             setTimeout(function () {
                 div.className = 'card finish';
                 cardFirst.className = 'card finish';
 
-                if (countUnit == 5) {
-                    // ゲーム終了
-                    setTimeout(function () {
-                        alert('界隈探求クリア！どう思った〜？');
-                    }, 500);   //　　0.5秒待って次の処理
-                } }, 300); }
-        else {   // 一致しない場合
+            // 色を設定
+            div.style.color = colors[currentColorIndex];
+            cardFirst.style.color = colors[currentColorIndex];
+            currentColorIndex = (currentColorIndex + 1) % colors.length; // インデックスを更新
+
+            if (countUnit == 5) {
+                // ゲーム終了
+                setTimeout(function () {
+                    alert('界隈探求クリア！どう思った〜？');
+                }, 500); }  //　　0.5秒後にアラート表示
+            isLocked = false; // ロックを解除
+        }, 500); }  // カードがペアになったら0.5秒の速度で文字の色が変わる
+        else {   // ノーペアの場合
             setTimeout(function () {
                 div.className = 'card back';
                 div.innerHTML = '';
                 cardFirst.className = 'card back';
                 cardFirst.innerHTML = '';
-                cardFirst = null; }, 700); }
+                cardFirst = null;
+                isLocked = false; // ロックを解除
+                cardFirst = null; }, 700); }  // 0.7秒の速度でノーペアのカードが伏せられる
         flgFirst = true;
     }
 }
