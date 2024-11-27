2ba7229c2dc2295971a0adf1d032189a357653a7
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-11-27 19:57:18 +0900

    本番環境に動的OGPを反映させる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,27 @@
 module ApplicationHelper
     def default_meta_tags
         {
-        site: 'あるある神経衰弱：界隈探求ゲーム',
-        title: 'あるある神経衰弱：界隈探求ゲーム',
+        site: '界隈探求ゲーム',
+        title: 'あるある神経衰弱',
         reverse: true,
         charset: 'utf-8',
         description: 'あらゆる界隈のあるあるを、AIで生成したり、みんなで投稿し遊ぶゲームです！',
         keywords: 'あるある,神経衰弱,界隈',
         canonical: request.original_url,
         separator: '|',
-        og: {
-            site_name: :site,
-            title: :title,
-            description: :description,
-            type: 'website',
-            url: request.original_url,
-            image: image_url('OGP.png'),
-            local: 'ja-JP'
-        },
+            og: {
+                site_name: :site,
+                title: :title,
+                description: :description,
+                type: 'website',
+                url: request.original_url,
+                image: image_url('OGP.png'),
+                local: 'ja-JP' },
 
-        twitter: {
-            card: 'summary_large_image',
-            site: '@https://x.com/pakira_rrrr',
-            image: image_url('OGP.png')
-        }
+            twitter: {
+                card: 'summary_large_image',
+                site: '@https://x.com/pakira_rrrr',
+                image: image_url('OGP.png') }
         }
     end
 end
