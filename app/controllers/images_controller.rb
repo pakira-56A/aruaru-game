8c637445af893a0bcd872390ffad789e99d56e17
pakira-56A <pakira.rrr@gmail.com>
2024-12-05 01:51:30 +0900

    ゲーム画面#4：動的OGPを本番環境で実装（OGPメソッドが走ってるかの確認）

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,10 +2,14 @@ class ImagesController < ApplicationController
   before_action :authenticate_user!, raise: false
 
   def ogp
-    text = ogp_params[:text]
-    image = OgpCreator.build(text).tempfile.open.read
-    send_data image, type: 'image/png', disposition: 'inline'
-    Rails.logger.info("ogpメソッドを実行")
+    begin
+      text = ogp_params[:text]
+      image = OgpCreator.build(text).tempfile.open.read
+      send_data image, type: 'image/png', disposition: 'inline'
+      Rails.logger.info("ogpメソッドを実行")
+    rescue StandardError => e
+      pp e
+    end
   end
 
   private
