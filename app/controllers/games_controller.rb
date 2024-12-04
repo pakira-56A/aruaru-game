e162e7c6d321aa0f6375d0bbed880b628c6502af
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-05 02:11:23 +0900

    ゲーム画面#4：動的OGPを本番環境で実装（OGPメソッドが走ってるかの確認2）

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,30 +3,29 @@ class GamesController < ApplicationController
 
   def start
     @post = Post.find(params[:id])
-    Rails.logger.info("ポストID: #{@post.id} のOGP画像を生成中")
-    begin
-      image_data = OgpCreator.build(prepare_meta_tags(@post))
-    rescue StandardError => e
-      Rails.logger.error("OGP画像生成中にエラーが発生しました: #{e.message}")
-      render json: { error: '内部サーバーエラー' }, status: :internal_server_error
-      return
-    end
-    Rails.logger.info("生成した画像データ: #{image_data.inspect}")
+    Rails.logger.info("ポストID: #{@post.id} を取得")
+    # begin
+    #   image_data = OgpCreator.build(prepare_meta_tags(@post))
+    # rescue StandardError => e
+    #   Rails.logger.error("OGP画像生成中にエラーが発生しました: #{e.message}")
+    #   render json: { error: '内部サーバーエラー' }, status: :internal_server_error
+    #   return
+    # end
+    # Rails.logger.info("生成した画像データ: #{image_data.inspect}")
   end
 
   private
 
   def authenticate_user!
     Rails.logger.info("#{request.user_agent}でアクセスしました")
-    return
-    # if request.user_agent =~ /bot|crawler|spider|Twitterbot/i
-    #   return
-    # end
-    # return if user_signed_in?
-    # # XシェアされたURLを未ログインユーザーがクリックした際、クリックしたURLを保存
-    # store_location_for(:user, request.original_url)
-    # flash[:alert] = 'ログインしてね〜！'
-    # redirect_to root_path
+    if request.user_agent =~ /bot|crawler|spider|Twitterbot/i
+      return
+    end
+    return if user_signed_in?
+    # XシェアされたURLを未ログインユーザーがクリックした際、クリックしたURLを保存
+    store_location_for(:user, request.original_url)
+    flash[:alert] = 'ログインしてね〜！'
+    redirect_to root_path
   end
 
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,13 +2,14 @@ class ImagesController < ApplicationController
   before_action :authenticate_user!, raise: false
 
   def ogp
+    Rails.logger.info("ogpメソッドを実行")
     begin
       text = ogp_params[:text]
       image = OgpCreator.build(text).tempfile.open.read
       send_data image, type: 'image/png', disposition: 'inline'
-      Rails.logger.info("ogpメソッドを実行")
+      Rails.logger.info("ogpのイメージをを出力")
     rescue StandardError => e
-      pp e
+      Rails.logger.error("OGP画像生成中にエラーが発生: #{e.message}")
     end
   end
 
