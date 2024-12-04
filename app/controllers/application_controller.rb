ecfccdd13fb4ab4aac1d0b127a63fadcd11d285b
pakira-56A <pakira.rrr@gmail.com>
2024-12-04 23:10:07 +0900

    ゲーム画面#4：動的OGPを本番環境で実装（prepare_meta_tagsをapplicationコントローラーに移動）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,36 @@
 class ApplicationController < ActionController::Base
   # deviseコントローラを使う前に、deviseコントローラーが真なら以下のアクションを実行する
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :prepare_meta_tags
 
   private
 
   def configure_permitted_parameters
     # 情報更新時にnameの取得を許可
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def prepare_meta_tags(post)
+    user_name = post.user.name
+    title = post.title
+    ogp_text = "#{user_name}さんが思う\n#{title}"
+    image_url = "#{request.base_url}/images/ogp.png?text=#{CGI.escape(ogp_text)}"
+
+    Rails.logger.info("『#{request.base_url}』でOGP画像の生成「#{user_name}さんの#{title}」画像URL「#{image_url}」")
+    set_meta_tags og: {
+                    site_name: 'あるある神経衰弱',
+                    title: post.title,
+                    description: 'この界隈あるあるで遊ぼう！',
+                    type: 'website',
+                    url: "#{request.base_url}/games/#{post.id}/start",
+                    image: image_url,
+                    locale: 'ja-JP'
+                  },
+                  twitter: {
+                    card: 'summary_large_image',
+                    site: '@https://x.com/pakira_rrr',
+                    image: image_url
+                  }
+    ogp_text # 生成したテキストを返す
   end
 end
