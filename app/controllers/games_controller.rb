e03c97d53ddd6c8630cbd3bb27b5cac486997cd7
pakira-56A <pakira.rrr@gmail.com>
2024-12-08 19:24:41 +0900

    投稿データに変更があれば、動的OGPを上書き。本番はS3、ローカルはfileに保存

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,40 +3,33 @@ class GamesController < ApplicationController
 
   def start
     @post = Post.find(params[:id])
-    prepare_meta_tags(@post)
     Rails.logger.info("ポストID: #{@post.id} を取得")
-    generate_and_save_ogp(@post)
+    set_meta_tags(og: { image: './app/assets/images/OGP_game.png' })
+    update_ogp_if_needed(@post)
   end
 
   private
 
+  # 前回生成したOGP画像と比較し、投稿者名や投稿タイトルに変更があれば上書き
+  def update_ogp_if_needed(post)
+    previous_ogp = post.ogp # 前回のOGP画像の情報を取得
+    if previous_ogp.nil? || post.user.name != post.previous_user_name || post.title != post.previous_title
+      generate_and_save_ogp(post) # 変更があればOGPを生成・保存
+    end
+  end
+
   def generate_and_save_ogp(post)
     begin
-      image_data = OgpCreator.build(prepare_meta_tags(post))
+      image_data = OgpCreator.build("#{post.user.name}さんが思う\n#{post.title}")
+      Rails.logger.info("ここ#{request.base_url}』でOGP画像の生成「#{post.user.name}さんの#{post.title}」画像URL「#{post.ogp.url}」")
     rescue StandardError => e
-      Rails.logger.error('動的OGP画像の生成に失敗')
+      Rails.logger.error("動的OGP画像の生成に失敗: #{e.message}")
       render json: { error: '内部サーバーエラー' }, status: :internal_server_error
       return
     end
 
-    Rails.logger.info("生成した動的OGP画像: #{image_data.inspect}")
-    return unless post.update!(ogp: image_data) # 生成したOGP画像をポストに登録
-
-    ogp_url = post.ogp.url
-    Rails.logger.info("生成した動的OGP画像を保存: #{ogp_url}")
-  end
-
-  def authenticate_user!
-    Rails.logger.info("アクセスしたのは#{request.user_agent}")
-    if request.user_agent =~ /bot|crawler|spider|Twitterbot/i
-      return
-    end
-    return if user_signed_in?
-
-    # XシェアされたURLを未ログインユーザーがクリックした際、クリックしたURLを保存
-    store_location_for(:user, request.original_url)
-    flash[:alert] = 'ログインしてね〜！'
-    redirect_to root_path
+    return unless post.update!(ogp: image_data, previous_user_name: post.user.name, previous_title: post.title) # 生成したOGP画像をポストに登録
+    Rails.logger.info("生成した動的OGP画像を保存: #{post.ogp.url}")
   end
 
 end
