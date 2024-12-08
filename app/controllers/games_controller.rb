b7373ae36217a6c5e278b31277b8cc2cd7a8167c
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-09 02:33:00 +0900

    12/9 投稿データに変更があれば、動的OGPを上書きを削除

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: %i[start]
 
   def start
     @post = Post.find(params[:id])
@@ -7,51 +6,15 @@ class GamesController < ApplicationController
     set_meta_tags(og: { image: "#{@post.ogp.url}"})
     set_meta_tags(twitter: { image: "#{@post.ogp.url}"})
     # set_meta_tags(og: { image: './app/assets/images/OGP_game.png' })
-    update_ogp_if_needed(@post)
-
+    generate_and_save_ogp(@post)
   end
 
   private
 
-  # 前回生成したOGP画像と比較し、投稿者名や投稿タイトルに変更があれば上書き
-  def update_ogp_if_needed(post)
-    previous_ogp = post.ogp # 前回のOGP画像の情報を取得
-    if previous_ogp.nil? || post.user.name != post.previous_user_name || post.title != post.previous_title
-      generate_and_save_ogp(post) # 変更があればOGPを生成・保存
-    end
-
-    # if previous_ogp.present?
-    #   # 存在している場合は、前回生成したOGP画像と比較し、投稿者名や投稿タイトルに変更があれば上書き
-    #   if user_name_changed?(post) || title_changed?(post)
-    #     generate_and_save_ogp(post) # 変更があればOGPを生成・保存
-    #   end
-    # else # なかったら作成・保存
-    #   generate_and_save_ogp(post)
-    # end
-
-    # if previous_ogp.present?
-    #   # 存在している場合は、前回生成したOGP画像と比較し、投稿者名や投稿タイトルに変更があれば上書き
-    #   if user_name_changed?(post) && title_changed?(post)
-    #     return # 変更があればOGPを生成・保存
-    #   end
-    #   # なかったら作成・保存
-    #   generate_and_save_ogp(post)
-    # end
-
-  end
-
-  # def user_name_changed?(post)
-  #   post.user.name != post.previous_user_name
-  # end
-
-  # def title_changed?(post)
-  #   post.title != post.previous_title
-  # end
-
   def generate_and_save_ogp(post)
     begin
       image_data = OgpCreator.build("#{post.user.name}さんが思う\n#{post.title}")
-      Rails.logger.info("ここ#{request.base_url}』でOGP画像の生成「#{post.user.name}さんの#{post.title}」画像URL「#{post.ogp.url}」")
+      Rails.logger.info("生成した動的OGP画像URL: #{post.ogp.url}")
     rescue StandardError => e
       Rails.logger.error("動的OGP画像の生成に失敗: #{e.message}")
       render json: { error: '内部サーバーエラー' }, status: :internal_server_error
@@ -62,7 +25,6 @@ class GamesController < ApplicationController
     Rails.logger.info("生成した動的OGP画像を保存: #{post.ogp.url}")
   rescue StandardError => e
     Rails.logger.error("生成した動的OGP画像を保存できませんでした: #{e.messages}")
-
   end
 
 end
--- app/uploaders/post_ogp_uploader.rb
+++ app/uploaders/post_ogp_uploader.rb
@@ -8,13 +8,13 @@ class PostOgpUploader < CarrierWave::Uploader::Base
   #   storage :file
   # end
 
+
   def store_dir
-    def store_dir
-      if Rails.env.production?
-        "#{mounted_as}_本番環境、投稿ID:#{model.id}"
-      else
-        "#{mounted_as}_ローカル、投稿ID:#{model.id}"
-      end
+    if Rails.env.production?
+      "#{mounted_as}_本番環境、投稿ID:#{model.id}"
+    else
+      "#{mounted_as}_ローカル、投稿ID:#{model.id}"
     end
   end
+
 end
