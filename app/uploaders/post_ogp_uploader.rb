976b620d37c2a329d7dd8b9947e4d279e6a3aa0f
pakira-56A <pakira.rrr@gmail.com>
2024-12-08 20:58:39 +0900

    マイグレーションファイルを追加し、カラムを入れ直した

--- app/uploaders/post_ogp_uploader.rb
+++ app/uploaders/post_ogp_uploader.rb
@@ -2,11 +2,11 @@ class PostOgpUploader < CarrierWave::Uploader::Base
 
   # storage :file
 
-  if Rails.env.production?
+  # if Rails.env.production?
     storage :fog
-  else
-    storage :file
-  end
+  # else
+  #   storage :file
+  # end
 
   def store_dir
     "#{mounted_as}_#{model.user.name}さんの#{model.title}（投稿ID:#{model.id}）"
--- db/migrate/20241205093128_add_ogp_to_post.rb
+++ db/migrate/20241205093128_add_ogp_to_post.rb
@@ -1,7 +1,5 @@
 class AddOgpToPost < ActiveRecord::Migration[7.1]
   def change
     add_column :posts, :ogp, :string
-    add_column :posts, :previous_user_name, :string
-    add_column :posts, :previous_title, :string
   end
 end
