79a9fa85491f09d17e4b8f15e083cdfe9716c7b3
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-08 20:58:39 +0900

    マイグレーションファイルを追加し、カラムを入れ直した

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,7 +16,34 @@ class GamesController < ApplicationController
     if previous_ogp.nil? || post.user.name != post.previous_user_name || post.title != post.previous_title
       generate_and_save_ogp(post) # 変更があればOGPを生成・保存
     end
+
+    # if previous_ogp.present?
+    #   # 存在している場合は、前回生成したOGP画像と比較し、投稿者名や投稿タイトルに変更があれば上書き
+    #   if user_name_changed?(post) || title_changed?(post)
+    #     generate_and_save_ogp(post) # 変更があればOGPを生成・保存
+    #   end
+    # else # なかったら作成・保存
+    #   generate_and_save_ogp(post)
+    # end
+
+    # if previous_ogp.present?
+    #   # 存在している場合は、前回生成したOGP画像と比較し、投稿者名や投稿タイトルに変更があれば上書き
+    #   if user_name_changed?(post) && title_changed?(post)
+    #     return # 変更があればOGPを生成・保存
+    #   end
+    #   # なかったら作成・保存
+    #   generate_and_save_ogp(post)
+    # end
+
   end
+
+  # def user_name_changed?(post)
+  #   post.user.name != post.previous_user_name
+  # end
+
+  # def title_changed?(post)
+  #   post.title != post.previous_title
+  # end
 
   def generate_and_save_ogp(post)
     begin
@@ -28,8 +55,12 @@ class GamesController < ApplicationController
       return
     end
 
+    # return unless post.update!(ogp: image_data, previous_user_name: post.user.name, previous_title: post.title) # 生成したOGP画像をポストに登録
     return unless post.update!(ogp: image_data, previous_user_name: post.user.name, previous_title: post.title) # 生成したOGP画像をポストに登録
     Rails.logger.info("生成した動的OGP画像を保存: #{post.ogp.url}")
+  rescue StandardError => e
+    Rails.logger.error("生成した動的OGP画像を保存できませんでした: #{e.messages}")
+
   end
 
 end
