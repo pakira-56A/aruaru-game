d1378f029f6af42e389b69db80477cceb3a81e69
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-10 15:56:38 +0900

    不要なカラムを削除するマイグレーションファイルを作成・実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_08_115314) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_10_064659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,8 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_08_115314) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ogp"
-    t.string "previous_user_name"
-    t.string "previous_title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
