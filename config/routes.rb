829b66cf9b0d94189f9e4773391dfe754212ea22
pakira-56A <pakira.rrr@gmail.com>
2024-12-13 21:03:53 +0900

    ransackで検索機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index new create show edit update destroy] do
     collection do
       get :myindex
+      get :search
     end
   end
 
