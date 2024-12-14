0a507e41dc8b1401fc022dcc4be75a67465daa8a
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-14 17:50:20 +0900

    `Posts`コントローラーのリファクタリング（`SearchPosts`コントローラーを生成） close #247 (#248)
    * 修正：ヘッダーとno_postのビューのCSS
    
    * rubocop-rails-omakaseをインストール
    
    * bin/rubocopが追加
    
    * 修正：Lintチェックで改行
    
    * 修正：Lintチェックで`'`を`"`に
    
    * `SearchPosts`コントローラーを生成し、切り分け
    
    * `SearchPosts`コントローラー用にビューを書換
    
    * `Posts`コントローラーのリファクタリング

---
+++ app/controllers/search_posts_controller.rb
@@ -0,0 +1,41 @@
+class SearchPostsController < ApplicationController
+    def search
+        @q = Post.ransack(params[:q])
+        @posts = @q.result(distinct: true)
+        respond_to do |format|
+            format.js
+            format.html { render :search }
+        end
+    end
+
+    def autocomplete
+        @posts = search_posts(params[:q])
+        respond_to do |format|
+            format.js
+            format.json { render json: @posts.pluck(:title) }
+        end
+    end
+
+    private
+
+        def search_posts(query)
+        conditions = [
+            "title ILIKE ?",
+            "title ILIKE ?",
+            "title ILIKE ?",
+            "title ILIKE ?",
+            "title ILIKE ?" ]
+
+        search_queries = [
+            "%#{query}%",
+            "%#{query.tr('ぁ-ん', 'ァ-ン')}%",
+            "%#{query.tr('ァ-ン', 'ぁ-ん')}%",
+            "%#{query.tr('一-龯', '')}%",
+            "%#{query.tr('a-zA-Z', '')}%" ]
+        posts = Post.where(conditions.join(" OR "), *search_queries)
+        if query.match?(/[a-zA-Z]/)
+            posts = posts.where("title ILIKE ?", "%#{query}%")
+        end
+        posts
+    end
+end
