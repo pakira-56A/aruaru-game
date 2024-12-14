2e34613377578ab0a913debf891a6eca1313d609
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-14 17:50:20 +0900

    `Posts`コントローラーのリファクタリング（`SearchPosts`コントローラーを生成） close #247 (#248)
    * 修正：ヘッダーとno_postのビューのCSS
    
    * rubocop-rails-omakaseをインストール
    
    * bin/rubocopが追加
    
    * 修正：Lintチェックで改行
    
    * 修正：Lintチェックで`'`を`"`に
    
    * `SearchPosts`コントローラーを生成し、切り分け
    
    * `SearchPosts`コントローラー用にビューを書換
    
    * `Posts`コントローラーのリファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[myindex new show edit update create]
+  before_action :set_post, only: %i[show edit update destroy]
 
   def index
     @q = Post.ransack(params[:q])
@@ -12,99 +13,65 @@ class PostsController < ApplicationController
 
   def myindex
     @q = current_user.posts.ransack(params[:q])
-    @posts = @q.result(distinct: true).where(user_id: current_user.id).includes(:user)
+    @posts = @q.result(distinct: true).includes(:user)
     render "users/posts/index"
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
+end
 
   def new
     @post = Post.new
   end
 
-  def edit
-    @post = current_user.posts.find(params[:id])
-  end
-
-  VALIDATION_MESSAGE = "全て入力してね！40文字までだよ！"
+  def edit; end
 
   def create
-    @post = current_user.posts.build(post_params)
-    if @post.save
-      flash[:notice] = "投稿したよー！遊んでもらおう！"
-      redirect_to myindex_posts_path
-    else
-      flash.now[:alert] = VALIDATION_MESSAGE
-      render :new, status: :unprocessable_entity
-    end
+      @post = current_user.posts.build(post_params)
+      save_post(:new)
   end
 
   def update
-    @post = current_user.posts.find(params[:id])
-    if @post.update(post_params)
-      flash[:notice] = "更新したよー！遊んでもらおう！"
-      redirect_to myindex_posts_path
-    else
-      flash.now[:alert] = VALIDATION_MESSAGE
-      render :edit, status: :unprocessable_entity
-    end
+      update_post
   end
 
   def destroy
     ActiveRecord::Base.transaction do
-      post = current_user.posts.find(params[:id])
-      post.destroy!
+      @post.destroy!
       flash[:notice] = "消したよ〜 また投稿してね！"
       redirect_to myindex_posts_path, status: :see_other
     end
-  rescue StandardError => e
-    flash[:alert] = "消すの失敗！ もう一度お試してみてね！"
+  rescue => e
+    flash[:alert] = "消すの失敗！ もう一度試してみてね！"
     redirect_to myindex_posts_path, status: :see_other
-  end
-
-  def search
-    @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true)
-    respond_to do |format|
-        format.js # JSリクエストに対応
-        format.html { render :search } # HTMLリクエストに対応
-    end
-  end
-
-  def autocomplete
-    @posts = search_posts(params[:q])
-    respond_to do |format|
-        format.js # JSリクエストに対応
-        format.json { render json: @posts.pluck(:title) }
-    end
   end
 
   private
 
-  def post_params
-    params.require(:post).permit(:title, :aruaru_one, :aruaru_two, :aruaru_three, :aruaru_four, :aruaru_five).merge(ogp: OgpCreator.build("#{current_user.name}さんが思う\n#{params[:post][:title]}"))
+  def set_post
+      @post = current_user.posts.find(params[:id]) if action_name.in?(%w[edit update destroy show])
   end
 
-  def search_posts(query)
-    conditions = [
-        "title ILIKE ?",
-        "title ILIKE ?",
-        "title ILIKE ?",
-        "title ILIKE ?",
-        "title ILIKE ?" ]
+  VALIDATION_MESSAGE = "全て入力してね！40文字までだよ！"
 
-    search_queries = [
-        "%#{query}%",
-        "%#{query.tr('ぁ-ん', 'ァ-ン')}%",
-        "%#{query.tr('ァ-ン', 'ぁ-ん')}%",
-        "%#{query.tr('一-龯', '')}%",
-        "%#{query.tr('a-zA-Z', '')}%" ]
-    posts = Post.where(conditions.join(" OR "), *search_queries)
-    if query.match?(/[a-zA-Z]/)
-        posts = posts.where("title ILIKE ?", "%#{query}%")
+  def save_post(action)
+    if @post.save
+        flash[:notice] = "投稿したよ！遊んでもらおう！"
+        redirect_to myindex_posts_path
+    else
+        flash.now[:alert] = VALIDATION_MESSAGE
+        render action, status: :unprocessable_entity
     end
-    posts
+  end
+
+  def update_post
+    if @post.update(post_params)
+        flash[:notice] = "更新したよ！遊んでもらおう！"
+        redirect_to myindex_posts_path
+    else
+        flash.now[:alert] = VALIDATION_MESSAGE
+        render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :aruaru_one, :aruaru_two, :aruaru_three, :aruaru_four, :aruaru_five).merge(ogp: OgpCreator.build("#{current_user.name}さんが思う\n#{params[:post][:title]}"))
   end
 end
