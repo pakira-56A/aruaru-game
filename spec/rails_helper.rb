6d360dc72633357c9e3a15f7a9e3404800c2b22b
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-19 20:23:17 +0900

    add: FactoryBot を設定・ファイルを生成/追記

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,10 +61,14 @@ RSpec.configure do |config|
   # behaviour is considered legacy and will be removed in a future version.
   #
   # To enable this behaviour uncomment the line below.
-  # config.infer_spec_type_from_file_location!
+  # テストファイル名やディレクトリ構成をみて、自動的にテストの種類を推測してくれる（一応有効にしておきます）
+  config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBotの設定をを追加
+  config.include FactoryBot::Syntax::Methods
 end
