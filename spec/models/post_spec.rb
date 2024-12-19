ba71b3ddee4f031adaa014e01928024a278708f8
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-19 20:14:12 +0900

    fix: `Post`モデルのテストケースを記載

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーションチェック' do
+    it '設定した全バリデーションが機能してるか' do end
+
+    it 'titleがない場合、バリデーションが機能してinvalidになるか' do end
+
+    it 'titleが41文字以上の場合、バリデーションが機能してinvalidになるか' do end
+
+    context "あるあるがない場合" do
+      it 'aruaru_oneがない場合、バリデーションが機能してinvalidになるか'  do end
+
+      it 'aruaru_twoがない場合、バリデーションが機能してinvalidになるか' do end
+
+      it 'aruaru_threeがない場合、バリデーションが機能してinvalidになるか' do end
+
+      it 'aruaru_fourがない場合、バリデーションが機能してinvalidになるか'  do end
+
+      it 'aruaru_fiveがない場合、バリデーションが機能してinvalidになるか'  do end
+    end
+
+    context "あるあるが41文字以上の場合" do
+      it 'aruaru_oneが41文字以上の場合、バリデーションが機能してinvalidになるか' do end
+
+      it 'aruaru_twoが41文字以上の場合、バリデーションが機能してinvalidになるか' do end
+
+      it 'aruaru_threeが41文字以上の場合、バリデーションが機能してinvalidになるか' do end
+
+      it 'aruaru_fourが41文字以上の場合、バリデーションが機能してinvalidになるか' do end
+
+      it 'aruaru_fiveが41文字以上の場合、バリデーションが機能してinvalidになるか' do end
+    end
+  end
 end
