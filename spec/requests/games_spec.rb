df6c7238069a4f4d319eaf17f24bb4186f5e09cc
pakira-56A <pakira.rrr@gmail.com>
2024-12-20 20:11:55 +0900

    fix: テストケース/テストコードを追記

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Games", type: :request do
     describe "GET /games/start/:id" do
-    pending "add some examples (or delete) #{__FILE__}"
+        let!(:post) { create(:post) } # FactoryBotを使ってポストを作成
+
+        it "正しいポストデータを取得する" do
+            get start_game_path(post.id)
+            expect(response.status).to eq(200)
+            expect(response).to be_successful  # レスポンスが成功であることを確認
+            expect(response.body).to include(post.title)  # 投稿データのタイトルが含まれているか確認
+        end
     end
 end
