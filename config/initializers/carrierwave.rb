24af9ced0e165e692ff0e1bd4024c2f05ef211e2
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-24 23:25:45 +0900

    GithubActions導入準備：carrierwaveで使うAWSの呼び出し方を`credentials`に戻す

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
         provider: "AWS",
-        aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-        region: ENV["S3_REGION"]
+        aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+        aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+        region: Rails.application.credentials.aws[:region]
     }
     config.fog_directory = "aruaru-game-bucket"
     config.fog_public = false
