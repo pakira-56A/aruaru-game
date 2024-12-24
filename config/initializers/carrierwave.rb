bebdc60b4397c7f5fea53fcb8d160d78a3c115e4
pakira-56A <pakira.rrr@gmail.com>
2024-12-25 01:33:28 +0900

    GithubActions導入準備：`credentials`に追加した環境変数を削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
         provider: "AWS",
         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-        region: ENV["AWS_REGION"]
+        region: "ap-northeast-1"
     }
-    config.fog_directory = ENV["AWS_BUCKET_NAME"]
+    config.fog_directory = "aruaru-game-bucket"
     config.fog_public = false
 end
