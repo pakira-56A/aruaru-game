cd53d860e7bf847921b872e062fe02caee97af87
pakira-56A <pakira.rrr@gmail.com>
2024-12-25 01:18:11 +0900

    GithubActions導入準備：Rspcテスト・`.env`での管理名を`AWS_`に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
         provider: "AWS",
-        aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-        region: ENV["S3_REGION"]
+        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region: ENV["AWS_REGION"]
     }
-    config.fog_directory = ENV["S3_BUCKET_NAME"]
+    config.fog_directory = ENV["AWS_BUCKET_NAME"]
     config.fog_public = false
 end
