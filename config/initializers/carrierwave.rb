a18bd327f037274437dee10b0ec32bacb88e099e
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-24 16:13:36 +0900

    Github actions 導入準備：S3の環境変数を`.env`から`credentials`に移動

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
         provider: "AWS",
-        aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-        region: ENV["S3_REGION"]
+        aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+        aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+        region: Rails.application.credentials.aws[:region]
     }
-    config.fog_directory = ENV["S3_BUCKET_NAME"]
+    config.fog_directory = Rails.application.credentials.aws[:bucket_name]
     config.fog_public = false
 end
