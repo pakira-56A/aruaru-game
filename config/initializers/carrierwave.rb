8a374fd592ff4a780886c5e8fa414cbe3e3c4f84
pakira-56A <pakira.rrr@gmail.com>
2024-12-25 00:12:33 +0900

    GithubActions導入準備：carrierwaveで使うAWSの呼び出し方を`ENV`に戻す2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
         aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
         region: ENV["S3_REGION"]
     }
-    config.fog_directory = "aruaru-game-bucket"
+    config.fog_directory = ENV["S3_BUCKET_NAME"]
     config.fog_public = false
 end
