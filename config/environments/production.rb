d23b275437dc1cc5e7b63a9e0607c99f3fd65c16
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-30 17:31:40 +0900

    fix: Lintチェック

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
   # 独自ドメインを設定
-  config.hosts << 'aruaru-games.com'
+  config.hosts << "aruaru-games.com"
 
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
