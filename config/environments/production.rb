39e7cf53f9aa32042dffb2003491c249d4047caa
pakira-56A <pakira.rrr@gmail.com>
2024-12-30 23:50:29 +0900

    fix: `config`の記載をコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  # 独自ドメインを設定
-  config.hosts << "www.aruaru-games.com"
+  # ホストを許可
+  # config.hosts << "www.aruaru-games.com"
   # config.hosts << "aruaru-game.onrender.com"
   # config.hosts = [
   #   "aruaru-games.com"     # Allow requests from example.com
