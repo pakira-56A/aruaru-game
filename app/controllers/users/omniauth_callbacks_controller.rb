f8441714f4ef160c1383024e8a63fd02542d8a28
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2024-12-31 19:40:16 +0900

    fix: Lintチェック、不要なコメント削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,43 +4,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # GoogleのOAuthコールバックを処理
   def google_oauth2
-    # callback_for(:google)
-
-    # @user = User.from_omniauth(request.env["omniauth.auth"])
-    # if @user.persisted?
-    #   sign_in_and_redirect @user, event: :authentication
-    #   set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-    # else
-    #   session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
-    #   redirect_to posts_path, alert: @user.errors.full_messages.join("\n")
-    # end
-
     # OmniAuthから認証データを取得して、モデルのメソッドを呼び出す
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       # ユーザーが存在する場合はログインしてリダイレクト
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
       # 新規ユーザーの保存が失敗した場合の処理
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # 不要なデータを削除
-      redirect_to new_user_registration_url, alert: 'Google認証に失敗しました。'
+      session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) # 不要なデータを削除
+      redirect_to new_user_registration_url, alert: "Google認証に失敗したよ"
     end
   end
-
-  # ユーザーをOAuth情報から作成・取得
-  # def callback_for(provider)
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, event: :authentication
-  #     set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
-  #   else
-  #     flash[:alert] = @user.errors.full_messages.to_sentence if @user.errors.any?
-  #     session["devise.#{provider}_data"] = request.env["omniauth.auth"].except(:extra)
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
 
   def failure
     redirect_to posts_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # database_authenticatable追加：パスワードなしでユーザー名を変更できる
   devise :database_authenticatable, :omniauthable, omniauth_providers: [ :google_oauth2 ]
-  # :database_authenticatable, :registerable,
-  # :recoverable, :rememberable, :validatable,
-  # :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
@@ -15,15 +10,6 @@ class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   has_many :posts, dependent: :destroy
-
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.name = auth.info.name
-  #     user.email = auth.info.email
-  #     # user.password = Devise.friendly_token[0, 20]
-  #     # user.skip_confirmation!  # ユーザーはメールをを受け取らずにアプリをを利用できる
-  #   end
-  # end
 
   def self.from_omniauth(auth)
     user = where(provider: auth.provider, uid: auth.uid).first_or_initialize
