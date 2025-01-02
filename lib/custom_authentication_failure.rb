742dd889141a085cbc8fbaa4a5743f9fa3dda2a3
pakira-56A <pakira.rrr@gmail.com>
2025-01-02 15:05:51 +0900

    add: `custom_authenticate_user!`の認証失敗時の処理

--- lib/custom_authentication_failure.rb
+++ lib/custom_authentication_failure.rb
@@ -1,7 +1,22 @@
 class CustomAuthenticationFailure < Devise::FailureApp
-  protected
+    protected
 
-  def redirect_url
-    root_path
-  end
+    def redirect_url
+        # Warden（Devise)が内部で使っている認証ライブラリ）のオプションの一部で、現在の認証スコープを示す
+        # ユーザーの種類やロールを特定するために使われる
+        if warden_options[:scope] == :user
+            root_path
+        else
+            super
+        end
+    end
+
+    def respond
+        # HTTP認証を使ってレスポンスを返すhttp_authメソッドを呼び出す
+        if http_auth?
+            http_auth
+        else
+            redirect
+        end
+    end
 end
