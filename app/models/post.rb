a8afb00f70aac6dd2b6e7cf2c35f4635623ae5dc
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2025-01-08 18:11:05 +0900

    add: モデル・以前AIが生成したお題か否かを判別。アンサーを５つに分けてあるあるに格納

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,9 @@ class Post < ApplicationRecord
 
   mount_uploader :ogp, PostOgpUploader
 
+  # AIが生成した投稿は検索対象から除外
+  scope :exclude_open_ai_answer, -> { where.not(user: User.find_by(name: "OPEN_AI_ANSWER")) }
+
   def self.ransackable_attributes(_auth_object = nil)
     [ "title" ]
   end
@@ -33,5 +36,52 @@ class Post < ApplicationRecord
       # tag_mapsテーブルにpost_idとtag_idを保存
       self.tags << new_post_tag
     end
+  end
+
+    # AIに生成させる場合（お題の文字数制限は、入力フォームで制限してます）
+    def self.find_or_create_from_openai(question)
+      # Railsのキャッシュでお題で以前作った投稿を探す。あれば取得、なければデータベースを探す。
+      cached_post = Rails.cache.fetch("openai_post_#{question}") do
+          # ユーザーが入力したお題で、AIが以前生成した投稿をデータベースから探す
+          find_by(title: question, user: User.find_by(name: "OPEN_AI_ANSWER"))
+      end
+
+      # キャッシュかデータベースから取得した投稿があれば、その投稿を返す
+      return cached_post if cached_post
+
+      # 見つからなければ、openai_service.rbの記載を使う
+      openai_response = OpenaiService.get_response(question)
+      # openai_service.rbで反応がなければ、nilを返す
+      return nil unless openai_response
+
+      # AIの返答を使って、あるあるを５つに切り分ける処理をする
+      answers = process_openai_response(openai_response)
+      # お題とAIが生成したあるあるを使って、新しい投稿を作成
+      post = create_from_answers(question, answers)
+
+      # 新規作成した投稿がデータベースに保存されたら、キャッシュにその投稿を記録
+      Rails.cache.write("openai_post_#{question}", post) if post.persisted?
+      # 新規作成した投稿を返す
+      post
+  end
+
+  private
+
+  # AIの返答を、あるあるを５つに切り分け。不要な記号などを除去
+  def self.process_openai_response(response)
+      response.split("。").map(&:strip)
+              .reject { |sentence| sentence.start_with?(".") }
+              .map { |sentence| sentence.sub(/^\d+\s*/, "") }
+  end
+
+  # お題とAIが生成したあるあるを使って、新しい投稿を作成
+  def self.create_from_answers(question, answers)
+      create(title: question,
+              user: User.find_or_create_by(name: "OPEN_AI_ANSWER"),
+              aruaru_one: answers[0],
+              aruaru_two: answers[1],
+              aruaru_three: answers[2],
+              aruaru_four: answers[3],
+              aruaru_five: answers[4])
   end
 end
