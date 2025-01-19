dfca2ea4a400ca2eedfba488d5b5f36412dd259a
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2025-01-19 16:06:01 +0900

    add: `likes`コントローラーとルーティング

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+    before_action :custom_authenticate_user!
+
+    def index
+        @likes = current_user.likes.includes(:post).order(created_at: :desc)
+        # @likesから取得した投稿の配列が入るけど、nilの投稿は除外されてる
+        @posts = @likes.map(&:post).compact
+    end
+
+    def create
+        @post = Post.find(params[:post_id])
+        current_user.like(@post)
+    end
+
+    def destroy
+        # postメソッドによってpostオブジェクトが取得
+        @post = current_user.likes.find(params[:id]).post
+        current_user.unlike(@post)
+    end
+end
