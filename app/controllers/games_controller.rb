4042f265fbb59b181308431948bbfb566ec606da
pakira-56A <167270907+pakira-56A@users.noreply.github.com>
2025-03-19 21:11:18 +0900

    fix: gameコントローラーに、動的OGP生成に特化したメソッドを用意

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,35 +1,33 @@
 class GamesController < ApplicationController
   def start
     @post = Post.find_by(id: params[:id])
-    if @post.nil?
-        handle_404
-        return
-    end
+    # 投稿がないなら、404メッセージを表示
+    return handle_404 if @post.nil?
 
     Rails.logger.info("ポストID: #{@post.id} を取得")
-    if @post.user.name != "OPEN_AI_ANSWER"
-      ogp_image_url = generate_and_save_ogp(@post)
-      set_meta_tags(og: { image: ogp_image_url }, twitter: { image: ogp_image_url })
-    else
-      Rails.logger.info("OGPを生成しない: AIが生成した投稿です")
-    end
+    # AIが生成した投稿でなければ、動的OGPを生成
+    generate_ogp if @post.user.name != "OPEN_AI_ANSWER"
   end
 
   private
 
-  def generate_and_save_ogp(post)
-    begin
-      image_data = OgpCreator.build("#{post.user.name}さんが思う\n#{post.title}")
-      # Rails.logger.info("生成した動的OGP画像URL: #{post.ogp.url}")
-
-      post.update!(ogp: image_data)
-      # 動的OGPに不具合があった時用に、loggerを残してます
-      Rails.logger.info("生成した動的OGP画像を保存: #{post.ogp.url}")
-      post.ogp.url
-
-    rescue StandardError => e
-      Rails.logger.error("動的OGP画像の生成 または保存に失敗: #{e.message}")
-      nil
+  def generate_ogp
+    ogp_image_url = generate_and_save_ogp(@post)
+    if ogp_image_url
+      set_meta_tags(og: { image: ogp_image_url }, twitter: { image: ogp_image_url })
+    else
+      Rails.logger.info("OGP生成に失敗した")
     end
+  end
+
+  def generate_and_save_ogp(post)
+    image_data = OgpCreator.build("#{post.user.name}さんが思う\n#{post.title}")
+    post.update!(ogp: image_data)
+    # 動的OGPに不具合があった時用に、loggerを残してます
+    Rails.logger.info("生成した動的OGP画像を保存: #{post.ogp.url}")
+    post.ogp.url
+  rescue StandardError => e
+    Rails.logger.error("動的OGP画像の生成 または保存に失敗: #{e.message}")
+    nil
   end
 end
